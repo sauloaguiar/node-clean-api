fffb0fd43e60941fe4ed75bbbac8e7e40e5cd7e3
Saulo <sauloaguiar89@gmail.com>
2020-12-29 11:38:18 -0300

    chore: adds jest configuration for various setups

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
