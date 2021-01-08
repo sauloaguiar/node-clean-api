b1b9c6f483e943b31c81b936c30fd8c41266e6f9
Saulo <sauloaguiar89@gmail.com>
2021-01-08 11:30:08 -0300

    feat: adds lib and mocked test to check for valid/invalid email

--- src/utils/validator/email-validator-adapter.spec.ts
+++ src/utils/validator/email-validator-adapter.spec.ts
@@ -1,10 +1,26 @@
 import { EmailValidatorAdapter } from './email-validator'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
 
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
+    // we test isolated from the dependencies
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid@email.com')
 
     expect(isValid).toBe(false)
+  })
+
+  test('Should return false if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid@email.com')
+
+    expect(isValid).toBe(true)
   })
 })
