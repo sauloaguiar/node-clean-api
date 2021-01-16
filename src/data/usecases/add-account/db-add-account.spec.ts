920675df1949df421553280fbce0f58ff0cbffdd
Saulo <sauloaguiar89@gmail.com>
2021-01-16 15:41:28 -0300

    feat: begins implementation of db-add-account usecase

---
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -0,0 +1,22 @@
+import { DbAddAccount } from '../add-account/db-add-account'
+
+describe('DbAddAccount UseCase', () => {
+  test('Should call encrypter with correct password', async () => {
+    class EncrypterStub {
+      async encrypt (value: string): Promise<string> {
+        return await new Promise(resolve => resolve('hashed_password'))
+      }
+    }
+    const encrypterStub = new EncrypterStub()
+    const sut = new DbAddAccount(encrypterStub)
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    await sut.add(accountData)
+
+    expect(encryptSpy).toHaveBeenCalledWith(accountData.password)
+  })
+})
