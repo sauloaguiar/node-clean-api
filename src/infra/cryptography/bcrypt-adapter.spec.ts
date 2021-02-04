a89fae4cbd29027c93bf99542e32e13f91db8c2e
Saulo <sauloaguiar89@gmail.com>
2021-02-04 10:21:53 -0300

    refactor: move sut creation to a factory helper

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -7,13 +7,17 @@ jest.mock('bcrypt', () => ({
   }
 }))
 
+const salt = 12
+
+const makeSut = (): BcryptAdapter => {
+  // sending salt as argument for the adapter instead of making it part of the protocol
+  // the adpater is specific to the library, the protocol is a generic abstraction of our application
+  return new BcryptAdapter(salt)
+}
+
 describe('BCrypt Adapter', () => {
   test('should call bcrypt with correct values', async () => {
-    const salt = 12
-
-    // sending salt as argument for the adapter instead of making it part of the protocol
-    // the adpater is specific to the library, the protocol is a generic abstraction of our application
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
 
     // we keep calling the function with one arg only as in the protocol
@@ -24,8 +28,7 @@ describe('BCrypt Adapter', () => {
   })
 
   test('should return a hash on success', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const hash = await sut.encrypt('any_value')
 
     // but we check that both values were given during the function call
