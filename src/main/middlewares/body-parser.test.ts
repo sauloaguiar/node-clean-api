595a38c50ae16667657f6b225e98be0cd06e444f
Saulo <sauloaguiar89@gmail.com>
2021-02-05 08:54:39 -0300

    feat: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+
+import app from '../config/app'
+
+describe('Body Parser middleware', () => {
+  it('Should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'Saulo' })
+      .expect({ name: 'Saulo' })
+  })
+})
