41098b15633ac1d7b0a7d5fac3a62749accbb201
Saulo <sauloaguiar89@gmail.com>
2021-02-04 22:25:09 -0300

    feat: ensure AccountMongoRepository returns account on success

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,20 @@
+import { Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+
+  async connect (url: string): Promise<void> {
+    this.client = await MongoClient.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect (): Promise<void> {
+    this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  }
+}
